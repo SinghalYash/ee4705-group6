c9c230f8bd4cf97b6ca26e8e8db79e731f92518e
savchann <savchanps89@gmail.com>
2026-09-14 12:41:38 +0800

    3 prong gripper

--- inspect_gripper.py
+++ inspect_gripper.py
@@ -9,35 +9,52 @@ GRIPPER_OPEN = 0.025
 
 
 def main():
-    model = mujoco.MjModel.from_xml_path(MODEL_PATH)
+
+    model = mujoco.MjModel.from_xml_path(
+        MODEL_PATH
+    )
+
     data = mujoco.MjData(model)
 
-    left_id = mujoco.mj_name2id(
+    finger_names = [
+        "act_finger_1",
+        "act_finger_2",
+        "act_finger_3",
+    ]
+
+    finger_ids = [
+        mujoco.mj_name2id(
+            model,
+            mujoco.mjtObj.mjOBJ_ACTUATOR,
+            name,
+        )
+        for name in finger_names
+    ]
+
+    # Open all three fingers.
+    for aid in finger_ids:
+        data.ctrl[aid] = GRIPPER_OPEN
+
+    with mujoco.viewer.launch_passive(
         model,
-        mujoco.mjtObj.mjOBJ_ACTUATOR,
-        "act_finger_left",
-    )
-
-    right_id = mujoco.mj_name2id(
-        model,
-        mujoco.mjtObj.mjOBJ_ACTUATOR,
-        "act_finger_right",
-    )
-
-    data.ctrl[left_id] = GRIPPER_OPEN
-    data.ctrl[right_id] = GRIPPER_OPEN
-
-    with mujoco.viewer.launch_passive(model, data) as viewer:
+        data,
+    ) as viewer:
 
         while viewer.is_running():
 
-            data.ctrl[left_id] = GRIPPER_OPEN
-            data.ctrl[right_id] = GRIPPER_OPEN
+            for aid in finger_ids:
+                data.ctrl[aid] = GRIPPER_OPEN
 
-            mujoco.mj_step(model, data)
+            mujoco.mj_step(
+                model,
+                data,
+            )
+
             viewer.sync()
 
-            time.sleep(model.opt.timestep)
+            time.sleep(
+                model.opt.timestep
+            )
 
 
 if __name__ == "__main__":
